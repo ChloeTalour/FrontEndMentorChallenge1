f75d524c1634386fd4c0522ad1dc0899f3715967
Chloé Talour <talour.chloe@gmail.com>
2020-10-12 21:06:15 +0200

    menu in progress

---
+++ src/components/Filo/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from 'src/components/Header';
+
+const Filo = () => (
+  <div className="filo">
+    <Header />
+  </div>
+);
+
+export default Filo;
---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Logo from '../../../images/logo.svg';
+import './styles.scss';
+
+const Header = () => (
+  <div className="header">
+    <div className="header__logo">
+      <a href="#">
+        <img className="header__logo--img" src={Logo} alt="Logo" />
+      </a>
+    </div>
+
+    <nav className="header__nav">
+      <ul className="header__nav--ul">
+        <li><a href="#">Features</a></li>
+        <li><a href="#">Team</a></li>
+        <li><a href="#">Sign In</a></li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Header;
